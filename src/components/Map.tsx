8aaf37e411ba188a766d5d05be809fd41f126ac8
JosuePrado <prado69508698@gmail.com>
2023-06-11 14:21:36 -0400

    Map Integration

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,10 @@ import L from 'leaflet';
 
 const map = L.map('map').setView([51.505, -0.09], 13);
 
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+// }).addTo(map);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
